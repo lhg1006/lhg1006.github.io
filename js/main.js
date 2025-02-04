c319f5bda31cca8ff62ef923c69601a87672e336
이효규 <lhg961006@gmail.com>
2025-02-04 09:38:09 +0900

    테스트

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// 스크롤 애니메이션
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+}); 
